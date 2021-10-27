39bfaf49f0d089c99d4091c1222905eec69d3d06
User 14 <User 14@PC15>
2021-10-27 14:45:11 +0300

    Add project files.

---
+++ WordCounter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int wordCount = 0, index = 0;
+
+
+            while (index < text.Length && char.IsWhiteSpace(text[index]))
+                index++;
+
+            while (index < text.Length)
+            {
+
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+
+            }
+            Console.WriteLine("Броят на думите е: " + wordCount);
+        }
+    }
+}
